fd1b010defd38a570b69196d019111b2d241b479
Jonte <jonatan@jontes.page>
2022-04-28 20:02:04 +0200

    Moving API endpoint from url to parameters

--- main.go
+++ main.go
@@ -1,31 +1,32 @@
+// Notes to Opensources:
+// - The code is really messy, but it works.
+// - The code is not optimized, but it works.
 package main
-
 import (
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"time"
 )
-
+// Gets URL parameter, then returns the latest Arch Linux release URL usiong the getLatestArchLinux function.
 func archLinux(w http.ResponseWriter, req *http.Request) {
-	setupCorsResponse(&w, req)
-	rackspaceurl := getLatestArchLinux("rackspace")
+	mirrors, ok := req.URL.Query()["mirror"]
+    if !ok || len(mirrors[0]) < 1 {
+        fmt.Fprint(w, "URL parameter 'mirror' is missing")
+        return
+    }
+    mirror := mirrors[0]
+	rackspaceurl := getLatestArchLinux(string(mirror))
 	fmt.Fprint(w, rackspaceurl)
-	timeOfRequest := time.Now()
-	fmt.Println("Incomming Request: ",req.Method, req.URL.Path, timeOfRequest.Format("01-02-2006 15:04:05"))
 }
-func setupCorsResponse(w *http.ResponseWriter, req *http.Request) {
-	(*w).Header().Set("Access-Control-Allow-Origin", "*")
-	(*w).Header().Set("Access-Control-Allow-Methods", "GET")
-	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length")
- }
+// Handles all requests to the server.
 func handleRequests() {
-	http.HandleFunc("/api/arch-rackspace-latest", archLinux)
+	http.HandleFunc("/api/arch", archLinux)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
+
+// Main Function for getting the latest Arch Linux release URL. 
 func getLatestArchLinux(mirror string) string {
-	fmt.Println(mirror)
 	resp, err := http.Get("https://mirror.rackspace.com/archlinux/iso/latest/arch/version")
 	if err != nil {
 		log.Fatalln(err)
@@ -35,14 +36,24 @@ func getLatestArchLinux(mirror string) string {
 		log.Fatalln(err)
 	}
 	sb := string(body)
-	outurl := "https://mirror.rackspace.com/archlinux/iso/latest/archlinux-" + sb + "-x86_64.iso"
+	outurl := "null"
+	if mirror == "rackspace" {
+		outurl := "https://mirror.rackspace.com/archlinux/iso/latest/archlinux-" + sb + "-x86_64.iso"
+		return outurl
+	} 
+	if mirror == "acc-umu" {
+		outurl := "https://ftp.acc.umu.se/mirror/archlinux/iso/" + sb + "/archlinux-" + sb + "-x86_64.iso"
+		return outurl
+	}
 	return outurl
 }
+
+// Main Function - Used for printing friendly messages to the server console.
 func main() {
 	fmt.Println("Jonte's Arch API Server - Written in Go.")
-	fmt.Println("Rackspace Mirror accessible at: http://localhost:8080/api/arch-rackspace-latest")
+	fmt.Println("Rackspace Mirror accessible at: http://localhost:8080/api/arch")
 	fmt.Println("Demo Accessible Here: https://jontes.page/api/arch")
 	fmt.Println("Starting the application...")
-	fmt.Println("Running Server on Port 8080")
+	fmt.Println("Running Server on Port 8080 (Or whatever you mapped your Docker container to)")
 	handleRequests()
 }
